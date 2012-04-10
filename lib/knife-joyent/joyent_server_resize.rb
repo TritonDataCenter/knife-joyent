eda4a0f3d34831026e95656cbd83722ab15537d6
Kevin Chan <kevin@joyent.com>
2012-04-10 22:49:43 +0800

    server_resize

---
+++ lib/knife-joyent/joyent_server_resize.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.dirname(__FILE__) + '/base')
+
+
+module KnifeJoyent
+  class JoyentServerResize < Chef::Knife
+
+    include KnifeJoyent::Base
+
+    banner 'knife joyent server resize <server_id> -f <flavor>'
+
+    option :flavor,
+      :short => "-f <flavor>",
+      :long => "--flavor <flavor>",
+      :description => "name of flavor/package to resize to"
+
+    def run
+      unless config[:flavor]
+        show_usage
+        exit 1
+      end
+
+      unless name_args.size === 1
+        show_usage
+        exit 1
+      end
+
+      id = name_args.first
+
+      server = self.connection.servers.get(id)
+      unless server
+        puts ui.error("Server with id: #{id} not found")
+        exit 1
+      end
+
+      if self.connection.resize_machine(id, config[:flavor])
+        puts ui.color("Resized server #{id}", :cyan)
+        exit 0
+      else
+        puts ui.error("Resize server failed")
+        exit 1
+      end
+    end
+  end
+end
