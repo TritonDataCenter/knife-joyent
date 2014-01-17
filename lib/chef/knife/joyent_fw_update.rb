452245398a45f9b73171e7cf8cc2724b5d89e0d5
Kevin Chan <kevin@yinkei.com>
2014-01-17 16:59:10 +0800

    fw rule required for update

--- lib/chef/knife/joyent_fw_update.rb
+++ lib/chef/knife/joyent_fw_update.rb
@@ -11,12 +11,14 @@ class Chef
 
       option :rule,
         :long => "--rule RULE",
-        :description => "Firewall Rule Content"
+        :description => "Firewall Rule Content",
+        :required => true
 
       option :enabled,
         :long => "--enabled",
         :boolean => true,
         :description => "Enable/Disable Rule"
+
 
       def run
         id = name_args.first
@@ -39,7 +41,7 @@ class Chef
         else
           r = res.body
 
-          ui.info "Created Firewall Rule: #{r["id"]}"
+          ui.info ui.color("Updated Firewall Rule: #{r["id"]}", :bold)
           msg_pair "RULE", r["rule"]
           msg_pair "ENABLED", (r["enabled"] ? ui.color("✓ YES", :cyan) : "✗ NO")
         end
